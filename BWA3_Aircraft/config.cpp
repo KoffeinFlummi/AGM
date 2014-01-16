79a8e3e989d5035f7b36a25fba695da73e711aa5
KoffeinFlummi <koffeinflummi@t-online.de>
2014-01-16 15:46:04 +0100

    Eject enabled for all aircraft seats + lock warnings

---
+++ BWA3_Aircraft/config.cpp
@@ -0,0 +1,243 @@
+class CfgPatches {
+  class BWA3_Aircraft {
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = 0.60;
+    requiredAddons[] = {A3_Weapons_F, A3_Anims_F};
+    version = 1.0;
+    author[] = {"KoffeinFLummi"};
+    authorUrl = "http://www.bwmod.de";
+  };
+};
+
+class CfgMods {
+  class BWA3_Aircraft {
+    dir = "BWA3_Aircraft";
+    name = "BWA3: Aircraft Module";
+    picture = "";
+    hidePicture = "true";
+    hideName = "true";
+    actionName = "Website";
+    action = "http://www.bwmod.de";
+  };
+};
+
+class Mode_Burst;
+
+// Manual Switching Of Flare Mode
+class CfgWeapons {
+  class SmokeLauncher;
+
+  class CMFlareLauncher: SmokeLauncher {
+    modes[] = {"Single", "Burst", "AIBurst"};
+    class Burst: Mode_Burst {
+      displayName = "Burst";
+    };
+  };
+};
+
+
+class CfgVehicles {
+  // Basic Inheritance
+  class AllVehicles;
+  class Air: AllVehicles {
+    class Turrets;
+  };
+  class Helicopter: Air {
+    class Turrets: Turrets {
+      class MainTurret;
+    };
+  };
+  class Helicopter_Base_F: Helicopter {
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {};
+    };
+  };
+  class Helicopter_Base_H: Helicopter_Base_F {
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {};
+      class CopilotTurret;
+    };
+  };
+
+  // BLUFOR Inheritance
+  class Heli_Attack_01_base_F: Helicopter_Base_F {
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {};
+    };
+  };
+  class Heli_Light_01_base_F: Helicopter_Base_H {
+    class Turrets: Turrets {
+      class CopilotTurret: CopilotTurret {};
+    };
+  };
+  class Heli_Light_01_armed_base_F: Heli_Light_01_base_F {
+    class Turrets: Turrets {
+      class CopilotTurret: CopilotTurret {};
+    };
+  };
+  class Heli_Transport_01_base_F: Helicopter_Base_H {
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {};
+      class CopilotTurret: CopilotTurret {};
+      class RightDoorGun;
+    };
+  };
+
+  // INDEP Inheritance
+  class I_Heli_light_03_base_F: Helicopter_Base_F {
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {};
+    };
+  };
+  class I_Heli_light_03_unarmed_base_F: I_Heli_light_03_base_F {
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {};
+    };
+  };
+  class Heli_Transport_02_base_F: Helicopter_Base_H {
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {};
+      class CopilotTurret: CopilotTurret {};
+    };
+  };
+  class Plane_Fighter_03_base_F;
+
+  // OPFOR Inheritance
+  class Heli_Light_02_base_F: Helicopter_Base_H {
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {};
+      class CopilotTurret: CopilotTurret {};
+    };
+  };
+  class Heli_Attack_02_base_F: Helicopter_Base_F {
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {};
+    };
+  };
+
+  // -------- //
+
+  // BLUFOR
+  class B_Heli_Attack_01_F: Heli_Attack_01_base_F {
+    lockDetectionSystem = 16;
+    driverCanEject = 1;
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {
+        canEject = 1;
+      };
+    };
+  };
+  class B_Heli_Transport_01_F: Heli_Transport_01_base_F {
+    lockDetectionSystem = 16;
+    driverCanEject = 1;
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {
+        canEject = 1;
+      };
+      class CopilotTurret: CopilotTurret {
+        canEject = 1;
+      };
+      class RightDoorGun: RightDoorGun {
+        canEject = 1;
+      };
+    };
+  };
+  class B_Heli_Light_01_F: Heli_Light_01_base_F {
+    lockDetectionSystem = 16;
+    driverCanEject = 1;
+    class Turrets: Turrets {
+      class CopilotTurret: CopilotTurret {
+        canEject = 1;
+      };
+    };
+  };
+  class B_Heli_Light_01_armed_F: Heli_Light_01_armed_base_F {
+    lockDetectionSystem = 16;
+    driverCanEject = 1;
+    class Turrets: Turrets {
+      class CopilotTurret: CopilotTurret {
+        canEject = 1;
+      };
+    };
+  };
+
+  // INDEP
+  class I_Heli_light_03_F: I_Heli_light_03_base_F {
+    lockDetectionSystem = 16;
+    driverCanEject = 1;
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {
+        canEject = 1;
+      };
+    };
+  };
+  class I_Heli_light_03_unarmed_F: I_Heli_light_03_unarmed_base_F {
+    lockDetectionSystem = 16;
+    driverCanEject = 1;
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {
+        canEject = 1;
+      };
+    };
+  };
+  class I_Heli_Transport_02_F: Heli_Transport_02_base_F {
+    lockDetectionSystem = 16;
+    driverCanEject = 1;
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {
+        canEject = 1;
+      };
+      class CopilotTurret: CopilotTurret {
+        canEject = 1;
+      };
+    };
+  };
+  class I_Plane_Fighter_03_CAS_F: Plane_Fighter_03_base_F {
+    lockDetectionSystem = 16;
+  };
+
+  // OPFOR
+  class O_Heli_Attack_02_F: Heli_Attack_02_base_F {
+    lockDetectionSystem = 16;
+    driverCanEject = 1;
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {
+        canEject = 1;
+      };
+    };
+  };
+  class O_Heli_Attack_02_black_F: Heli_Attack_02_base_F {
+    lockDetectionSystem = 16;
+    driverCanEject = 1;
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {
+        canEject = 1;
+      };
+    };
+  };
+  class O_Heli_Light_02_F: Heli_Light_02_base_F {
+    lockDetectionSystem = 16;
+    driverCanEject = 1;
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {
+        canEject = 1;
+      };
+      class CopilotTurret: CopilotTurret {
+        canEject = 1;
+      };
+    };
+  };
+  class O_Heli_Light_02_unarmed_F: Heli_Light_02_base_F {
+    lockDetectionSystem = 16;
+    driverCanEject = 1;
+    class Turrets: Turrets {
+      class MainTurret: MainTurret {
+        canEject = 1;
+      };
+      class CopilotTurret: CopilotTurret {
+        canEject = 1;
+      };
+    };
+  };
+};
