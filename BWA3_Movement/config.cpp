3d2f8345835d54ec9e6945fb678539179462a71c
KoffeinFlummi <koffeinflummi@t-online.de>
2014-02-22 16:17:21 +0100

    Different walking animation

--- BWA3_Movement/config.cpp
+++ BWA3_Movement/config.cpp
@@ -65,13 +65,6 @@ class CfgMovesMaleSdr: CfgMovesBasic {
     class RifleStandEvasiveActionsFR: RifleStandActionsNoAdjust {
       getOver = "AovrPercMrunSrasWrflDf";
     };
-    // WEAPON LOWERED - WALKING
-    /* doesn't work for some reason
-    class RifleLowStandActionsWlkF: RifleLowStandActionsNoAdjust {
-      PlayerWalkF = "AmovPercMwlkSlowWrflDf_v2";
-      WalkF = "AmovPercMwlkSlowWrflDf_v2";
-    };
-    */
     // WEAPON LOWERED - RUNNING
     class RifleLowStandActionsRunF: RifleLowStandActionsNoAdjust {
       getOver = "AovrPercMrunSrasWrflDf";
@@ -82,12 +75,27 @@ class CfgMovesMaleSdr: CfgMovesBasic {
     class RifleLowStandActionsRunFR: RifleLowStandActionsNoAdjust {
       getOver = "AovrPercMrunSrasWrflDf";
     };
+
+    // WEAPON LOWERED - WALKING
+    /*
+    class RifleLowStandActionsWlkF: RifleLowStandActionsNoAdjust {
+      PlayerWalkF = "AmovPercMwlkSlowWrflDf_v2";
+      WalkF = "AmovPercMwlkSlowWrflDf_v2";
+    };
+    */
   };
   class States {
     class AmovPercMstpSrasWrflDnon;
+    class AmovPercMstpSlowWrflDnon;
 
     class AmovPercMrunSrasWrflDf: AmovPercMstpSrasWrflDnon { 
       InterpolateTo[] = {"AovrPercMrunSrasWrflDf",0.22,"AmovPercMrunSlowWrflDf",0.025,"AmovPercMwlkSrasWrflDf",0.025,"AmovPknlMrunSrasWrflDf",0.03,"AmovPercMrunSlowWrflDf_AmovPpneMstpSrasWrflDnon",0.02,"AmovPercMevaSrasWrflDf",0.025,"AmovPercMrunSlowWrflDf_AmovPercMevaSrasWrflDl",0.05,"AmovPercMrunSlowWrflDf_AmovPercMevaSrasWrflDr",0.05,"AmovPercMrunSlowWrflDf_AmovPercMevaSrasWrflDb",0.05,"Unconscious",0.01,"AmovPercMtacSrasWrflDf",0.02,"AmovPercMrunSrasWrflDfl",0.02,"AmovPercMrunSrasWrflDfl_ldst",0.02,"AmovPercMrunSrasWrflDfr",0.02,"AmovPercMrunSrasWrflDfr_ldst",0.02,"AmovPercMstpSrasWrflDnon",0.02,"AmovPercMrunSrasWrflDl",0.02,"AmovPercMrunSrasWrflDbl",0.02,"AmovPercMrunSrasWrflDb",0.02,"AmovPercMrunSrasWrflDbr",0.02,"AmovPercMrunSrasWrflDr",0.02,"AmovPknlMstpSlowWrflDnon_relax",0.1,"AmovPercMrunSrasWrflDf_ldst",0.02,"AmovPercMrunSrasWrflDf",0.02};
+    };
+
+    class AmovPercMwlkSlowWrflDf: AmovPercMstpSlowWrflDnon {
+      speed = 0.206897;
+      file = "\A3\anims_f\Data\Anim\Sdr\Mov\Erc\Wlk\Low\Rfl\AmovPercMwlkSlowWrflDf_ver2";
+      leftHandIKCurve[] = {1};
     };
   };
 };
