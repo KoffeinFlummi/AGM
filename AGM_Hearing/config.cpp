9e34c99e8244125df86738437d957d60596a56de
commy2 <d802099@drdrb.com>
2014-06-05 21:50:30 +0200

    fixed error in overheating

--- AGM_Hearing/config.cpp
+++ AGM_Hearing/config.cpp
@@ -27,7 +27,7 @@ class CfgFunctions {
 class Extended_Init_EventHandlers {
   class CAManBase {
     class AGM_giveSpareBarrel {
-      clientInit = "_this spawn {sleep 0.1; _ammo = getText (configFile >> 'CfgMagazines' >> currentMagazine player >> 'ammo'); if (getNumber (configFile >> 'CfgMagazines' >> _ammo >> 'audiblefire') > 8) then {(_this select 0) addItem 'AGM_EarBuds'}}"; 
+      clientInit = "_this spawn {sleep 0.1; _ammo = getText (configFile >> 'CfgMagazines' >> currentMagazine player >> 'ammo'); if (getNumber (configFile >> 'CfgAmmo' >> _ammo >> 'audiblefire') > 8) then {(_this select 0) addItem 'AGM_EarBuds'}}"; 
     };
   };
 };
--- AGM_Overheating/config.cpp
+++ AGM_Overheating/config.cpp
@@ -67,7 +67,7 @@ class CfgVehicles {
       class AGM_SwapBarrel {
         displayName = "$STR_AGM_Overheating_SwapBarrel";
         condition = "'AGM_SpareBarrel' in items player && {getNumber (configFile >> 'CfgWeapons' >> currentWeapon player >> 'AGM_Overheating_allowSwapBarrel') == 1}";
-        statement = "[currentWeapon player] call AGM_Overheating_swapBarrel";
+        statement = "[currentWeapon player] call AGM_Overheating_fnc_swapBarrel";
         showDisabled = 0;
         priority = 4.1;
       };
