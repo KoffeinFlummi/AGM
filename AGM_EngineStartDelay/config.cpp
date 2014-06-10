6c7ed0e0ef80bdaddd7b6c2638cd24490634ccce
commy2 <d802099@drdrb.com>
2014-06-10 02:37:21 +0200

    fixed cfgPatches

--- AGM_EngineStartDelay/config.cpp
+++ AGM_EngineStartDelay/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches {
     units[] = {};
     weapons[] = {};
     requiredVersion = 0.60;
-    requiredAddons[] = {A3_Dubbing_Radio_F};
+    requiredAddons[] = {Extended_EventHandlers};
     version = "0.9";
     versionStr = "0.9";
     versionAr[] = {0,9,0};
