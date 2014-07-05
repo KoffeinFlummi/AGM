0e629cb4dbc3c480eb7209ce0ff911d390cf82e0
KoffeinFlummi <koffeinflummi@gmail.com>
2014-07-05 22:53:09 +0200

    Fixed #55

--- AGM_Flares/config.cpp
+++ AGM_Flares/config.cpp
@@ -39,3 +39,61 @@ class CfgAmmo {
     flareSize = 12;
   };
 };
+
+class CfgMagazines {
+  class HandGrenade;
+
+  class AGM_HandFlare_Red: HandGrenade {
+    ammo = "F_20mm_Red";
+    displayname = "Hand Flare (Red)";
+    descriptionshort = "Red Hand Flare";
+    displayNameShort = "Red Flare";
+    model = "\A3\weapons_f\ammo\flare_red";
+    picture = "\A3\Weapons_F\Data\UI\gear_flare_red_ca.paa";
+  };
+  class AGM_HandFlare_Yellow: HandGrenade {
+    ammo = "F_20mm_Yellow";
+    displayname = "Hand Flare (Yellow)";
+    descriptionshort = "Yellow Hand Flare";
+    displayNameShort = "Yellow Flare";
+    model = "\A3\weapons_f\ammo\flare_yellow";
+    picture = "\A3\Weapons_F\Data\UI\gear_flare_yellow_ca.paa";
+  };
+  class AGM_HandFlare_Green: HandGrenade {
+    ammo = "F_20mm_Green";
+    displayname = "Hand Flare (Green)";
+    descriptionshort = "Green Hand Flare";
+    displayNameShort = "Green Flare";
+    model = "\A3\weapons_f\ammo\flare_green";
+    picture = "\A3\Weapons_F\Data\UI\gear_flare_green_ca.paa";
+  };
+  class AGM_HandFlare_White: HandGrenade {
+    ammo = "F_20mm_White";
+    displayname = "Hand Flare (White)";
+    descriptionshort = "White Hand Flare";
+    displayNameShort = "White Flare";
+    model = "\A3\weapons_f\ammo\flare_white";
+    picture = "\A3\Weapons_F\Data\UI\gear_flare_white_ca.paa";
+  };
+};
+
+class CfgWeapons {
+  class GrenadeLauncher;
+
+  class Throw: GrenadeLauncher {
+    muzzles[] += {"AGM_HandFlare_RedMuzzle", "AGM_HandFlare_YellowMuzzle", "AGM_HandFlare_GreenMuzzle", "AGM_HandFlare_WhiteMuzzle"};
+    class ThrowMuzzle;
+    class AGM_HandFlare_RedMuzzle: ThrowMuzzle {
+      magazines[] = {"AGM_HandFlare_Red"};
+    };
+    class AGM_HandFlare_YellowMuzzle: ThrowMuzzle {
+      magazines[] = {"AGM_HandFlare_Yellow"};
+    };
+    class AGM_HandFlare_GreenMuzzle: ThrowMuzzle {
+      magazines[] = {"AGM_HandFlare_Green"};
+    };
+    class AGM_HandFlare_WhiteMuzzle: ThrowMuzzle {
+      magazines[] = {"AGM_HandFlare_White"};
+    };
+  };
+};
