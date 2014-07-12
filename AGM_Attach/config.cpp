cd28b94a36727ae179c1a094f9d0bbe2e0348c79
commy2 <d802099@drdrb.com>
2014-07-12 21:24:26 +0200

    put conditions in AGM_Attach in functions: see #452, basic formating

--- AGM_Attach/config.cpp
+++ AGM_Attach/config.cpp
@@ -17,6 +17,8 @@ class CfgFunctions {
     class AGM_Attach {
       file = "AGM_Attach\functions";
       class attach;
+      class canAttach;
+      class canDetach;
       class detach;
     };
   };
@@ -29,87 +31,87 @@ class CfgVehicles {
 
       class AGM_Attach {
         displayName = "$STR_AGM_Attach_AttachDetach";
-        condition = "canStand player && {alive player}";
-        statement = "'AGM_Attach' call AGM_Interaction_fnc_openMenuSelf;";
-        showDisabled = 1;
+        condition = "[''] call AGM_Attach_fnc_canAttach";
+        statement = "'AGM_Attach' call AGM_Interaction_fnc_openMenuSelf";
+        showDisabled = 0;
         priority = -10;
 
-        class AGM_Attach_Detach {
-          displayName = "$STR_AGM_Attach_Detach";
-          condition = "canStand player && {alive player} && {player getVariable ['AGM_AttachedItemName', ''] != ''}";
-          statement = "player call AGM_Attach_fnc_detach;";
-          showDisabled = 1;
-          priority = 10;
-        };
         class AGM_Attach_IrStrobe {
           displayName = "$STR_AGM_Attach_Attach_IrStrobe";
-          condition = "canStand player && {alive player} && {player getVariable ['AGM_AttachedItemName', ''] == ''} && {'AGM_IR_Strobe_Item' in items player}";
-          statement = "[player, 'AGM_IR_Strobe_Item'] call AGM_Attach_fnc_attach;";
+          condition = "['AGM_IR_Strobe_Item'] call AGM_Attach_fnc_canAttach";
+          statement = "[player, 'AGM_IR_Strobe_Item'] call AGM_Attach_fnc_attach";
           showDisabled = 0;
           priority = 9;
         };
         class AGM_Attach_IrStrobeB {
           displayName = "$STR_AGM_Attach_Attach_IrGrenade";
-          condition = "canStand player && {alive player} && {player getVariable ['AGM_AttachedItemName', ''] == ''} && {'B_IR_Grenade' in magazines player}";
-          statement = "[player, 'B_IR_Grenade'] call AGM_Attach_fnc_attach;";
+          condition = "['B_IR_Grenade'] call AGM_Attach_fnc_canAttach";
+          statement = "[player, 'B_IR_Grenade'] call AGM_Attach_fnc_attach";
           showDisabled = 0;
           priority = 8;
         };
         class AGM_Attach_IrStrobeO {
           displayName = "$STR_AGM_Attach_Attach_IrGrenade";
-          condition = "canStand player && {alive player} && {player getVariable ['AGM_AttachedItemName', ''] == ''} && {'O_IR_Grenade' in magazines player} && {!('B_IR_Grenade' in magazines player)}";
-          statement = "[player, 'O_IR_Grenade'] call AGM_Attach_fnc_attach;";
+          condition = "['O_IR_Grenade'] call AGM_Attach_fnc_canAttach && {!('B_IR_Grenade' in magazines player)}";
+          statement = "[player, 'O_IR_Grenade'] call AGM_Attach_fnc_attach";
           showDisabled = 0;
           priority = 8;
         };
         class AGM_Attach_IrStrobeI {
           displayName = "$STR_AGM_Attach_Attach_IrGrenade";
-          condition = "canStand player && {alive player} && {player getVariable ['AGM_AttachedItemName', ''] == ''} && {'I_IR_Grenade' in magazines player} && {!('B_IR_Grenade' in magazines player)} && {!('O_IR_Grenade' in magazines player)}";
-          statement = "[player, 'I_IR_Grenade'] call AGM_Attach_fnc_attach;";
+          condition = "['I_IR_Grenade'] call AGM_Attach_fnc_canAttach && {!('B_IR_Grenade' in magazines player)} && {!('O_IR_Grenade' in magazines player)}";
+          statement = "[player, 'I_IR_Grenade'] call AGM_Attach_fnc_attach";
           showDisabled = 0;
           priority = 8;
         };
         class AGM_Attach_Chemlight_blue {
           displayName = "$STR_AGM_Attach_Attach_Chemlight_blue";
-          condition = "canStand player && {alive player} && {player getVariable ['AGM_AttachedItemName', ''] == ''} && {'Chemlight_blue' in magazines player}";
-          statement = "[player, 'Chemlight_blue'] call AGM_Attach_fnc_attach;";
+          condition = "['Chemlight_blue'] call AGM_Attach_fnc_canAttach";
+          statement = "[player, 'Chemlight_blue'] call AGM_Attach_fnc_attach";
           showDisabled = 0;
           priority = 7;
         };
         class AGM_Attach_Chemlight_green {
           displayName = "$STR_AGM_Attach_Attach_Chemlight_green";
-          condition = "canStand player && {alive player} && {player getVariable ['AGM_AttachedItemName', ''] == ''} && {'Chemlight_green' in magazines player}";
-          statement = "[player, 'Chemlight_green'] call AGM_Attach_fnc_attach;";
+          condition = "['Chemlight_green'] call AGM_Attach_fnc_canAttach";
+          statement = "[player, 'Chemlight_green'] call AGM_Attach_fnc_attach";
           showDisabled = 0;
           priority = 7;
         };
         class AGM_Attach_Chemlight_red {
           displayName = "$STR_AGM_Attach_Attach_Chemlight_red";
-          condition = "canStand player && {alive player} && {player getVariable ['AGM_AttachedItemName', ''] == ''} && {'Chemlight_red' in magazines player}";
-          statement = "[player, 'Chemlight_red'] call AGM_Attach_fnc_attach;";
+          condition = "['Chemlight_red'] call AGM_Attach_fnc_canAttach";
+          statement = "[player, 'Chemlight_red'] call AGM_Attach_fnc_attach";
           showDisabled = 0;
           priority = 7;
         };
         class AGM_Attach_Chemlight_yellow {
           displayName = "$STR_AGM_Attach_Attach_Chemlight_yellow";
-          condition = "canStand player && {alive player} && {player getVariable ['AGM_AttachedItemName', ''] == ''} && {'Chemlight_yellow' in magazines player}";
-          statement = "[player, 'Chemlight_yellow'] call AGM_Attach_fnc_attach;";
+          condition = "['Chemlight_yellow'] call AGM_Attach_fnc_canAttach";
+          statement = "[player, 'Chemlight_yellow'] call AGM_Attach_fnc_attach";
           showDisabled = 0;
           priority = 7;
         };
+      };
+
+      class AGM_Attach_Detach {
+        displayName = "$STR_AGM_Attach_Detach";
+        condition = "call AGM_Attach_fnc_canDetach";
+        statement = "[player] call AGM_Attach_fnc_detach";
+        showDisabled = 0;
+        priority = -10;
       };
     };
   };
 
   class All;
-  class AGM_IR_Strobe_Effect: All
-  {
+  class AGM_IR_Strobe_Effect: All {
     scope = 1;
     displayName = "IR Strobe";
     model = "\A3\Weapons_F\empty.p3d";
     simulation = "nvmarker";
-    class NVGMarker
-    {
+
+    class NVGMarker {
       diffuse[] = {0.01, 0.01, 0.01, 1};
       ambient[] = {0.005, 0.005, 0.005, 1};
       brightness = 0.2;
