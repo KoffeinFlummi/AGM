e61132f7077f9be96631b4ad74786c835414392c
commy2 <d802099@drdrb.com>
2014-08-08 13:14:23 +0200

    fixed #746

--- AGM_Overheating/config.cpp
+++ AGM_Overheating/config.cpp
@@ -154,6 +154,31 @@ class CfgVehicles {
       MACRO_ADDITEM(AGM_SpareBarrel,6)
     };
   };
+
+  // Add AGM_SpareBarrel to every machine gunner.
+  #define MACRO_ADDSPAREBARREL \
+    items[] = {"FirstAidKit","AGM_SpareBarrel"}; \
+    respawnitems[] = {"FirstAidKit","AGM_SpareBarrel"};
+
+  // NATO
+  class B_Soldier_base_F; class B_soldier_AR_F:B_Soldier_base_F {MACRO_ADDSPAREBARREL};
+  class B_Soldier_support_base_F; class B_soldier_AAR_F:B_Soldier_support_base_F {MACRO_ADDSPAREBARREL};
+  class B_CTRG_soldier_AR_A_F:B_Soldier_base_F {MACRO_ADDSPAREBARREL};
+
+  // Guerrilla
+  class I_G_Soldier_base_F; class I_G_Soldier_AR_F:I_G_Soldier_base_F {MACRO_ADDSPAREBARREL};
+  class B_G_Soldier_AR_F:I_G_Soldier_AR_F {MACRO_ADDSPAREBARREL};
+  class O_G_Soldier_AR_F:I_G_Soldier_AR_F {MACRO_ADDSPAREBARREL};
+
+  // Iran
+  class O_Soldier_base_F; class O_Soldier_AR_F:O_Soldier_base_F {MACRO_ADDSPAREBARREL};
+  class O_Soldier_support_base_F; class O_Soldier_AAR_F:O_Soldier_support_base_F {MACRO_ADDSPAREBARREL};
+  class O_Soldier_Urban_base; class O_soldierU_AR_F:O_Soldier_Urban_base {MACRO_ADDSPAREBARREL};
+  class O_soldierU_AAR_F:O_Soldier_Urban_base {MACRO_ADDSPAREBARREL};
+
+  // Czech
+  class I_Soldier_base_F; class I_Soldier_AR_F:I_Soldier_base_F {MACRO_ADDSPAREBARREL};
+  class I_Soldier_support_base_F; class I_Soldier_AAR_F:I_Soldier_support_base_F {MACRO_ADDSPAREBARREL};
 };
 
 class CfgWeapons {
