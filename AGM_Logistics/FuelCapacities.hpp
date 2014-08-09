e81206a4128379865052a13adc408692d3ff5573
KoffeinFlummi <koffeinflummi@gmail.com>
2014-08-10 00:00:11 +0200

    More realistic fuel values.

--- AGM_Logistics/FuelCapacities.hpp
+++ AGM_Logistics/FuelCapacities.hpp
@@ -1,6 +1,4 @@
-
-// @todo realistic values
-#define AGM_FUELCAPACITY_KART            8
+#define AGM_FUELCAPACITY_KART            4
 #define AGM_FUELCAPACITY_QUAD            15
 #define AGM_FUELCAPACITY_OFFROARD        80    // http://www.nissan.de/content/dam/services/DE/brochure/NISSAN_Navara_Broschuere+Preisliste_DE.pdf
 #define AGM_FUELCAPACITY_VAN             80    // @todo
@@ -15,18 +13,18 @@
 
 #define AGM_FUELCAPACITY_PATRIA          600   // @todo
 #define AGM_FUELCAPACITY_ARMA            600   // @todo
-#define AGM_FUELCAPACITY_PANDUR          600   // @todo
+#define AGM_FUELCAPACITY_PANDUR          228   // http://www.pandur.at/panzer2.htm
 
-#define AGM_FUELCAPACITY_NAMER           800   // @todo
+#define AGM_FUELCAPACITY_NAMER           1400  // assumed to be the same as the merkava
 #define AGM_FUELCAPACITY_BMP             800   // @todo
-#define AGM_FUELCAPACITY_WARRIOR         800   // @todo
+#define AGM_FUELCAPACITY_WARRIOR         770   // http://arcaneafvs.com/warrior.html (don't ask)
 
-#define AGM_FUELCAPACITY_MERKAVA         1400
+#define AGM_FUELCAPACITY_MERKAVA         1400  // http://en.wikipedia.org/wiki/Merkava
 #define AGM_FUELCAPACITY_T100            1600  // http://www.tanksim.com/topic3.htm
-#define AGM_FUELCAPACITY_LEOPARD         1160
+#define AGM_FUELCAPACITY_LEOPARD         1200  // http://en.wikipedia.org/wiki/Leopard_2
 
-#define AGM_FUELCAPACITYCARGO_VAN        1000  // @todo
+#define AGM_FUELCAPACITYCARGO_VAN        5000  // guesswork based on relative size compared to the othes
 #define AGM_FUELCAPACITYCARGO_HEMTT      9464  // http://oshkoshdefense.com/variants/m978a4-fuel-servicing-truck-tanker/
-#define AGM_FUELCAPACITYCARGO_KAMAZ      3000  // @todo
-#define AGM_FUELCAPACITYCARGO_TYPHOON    3000  // @todo
-#define AGM_FUELCAPACITYCARGO_CRV        1500  // @todo
+#define AGM_FUELCAPACITYCARGO_KAMAZ      7125  // guesswork based on relative size compared to HEMTT
+#define AGM_FUELCAPACITYCARGO_TYPHOON    7125  // guesswork based on relative size compared to HEMTT
+#define AGM_FUELCAPACITYCARGO_CRV        0     // this thing doesn't carry any fuel (I think)
