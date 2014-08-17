95b870530c343a00157024e82e8716585661680f
Garth "L-H" de Wet <garthofhearts@gmail.com>
2014-08-17 23:17:19 +0200

    Added showMouseHint/hideMouseHint which displays a left click/right click icon with text.

--- AGM_Interaction/Menu_Config.hpp
+++ AGM_Interaction/Menu_Config.hpp
@@ -542,4 +542,46 @@ class RscTitles {
       };
     };
   };
+  class AGM_InteractionHelper {
+    idd = 9930;
+    enableSimulation = 1;
+    movingEnable = 0;
+    fadeIn=0.5;
+    fadeOut=0.5;
+    duration = 10e10;
+    onLoad = "uiNamespace setVariable ['AGM_Helper_Display', _this select 0];";
+
+    class controls {
+      class SelectIcon: RscInteractionHelperIcon{
+        idc = 1200;
+        text = "AGM_interaction\UI\mouse_left_ca.paa";
+        y = 17.5 * GUI_GRID_H;
+      };
+      class SelectText: RscInteractionText{
+        idc = 1000;
+        y = 17 * GUI_GRID_H;
+        text = $STR_AGM_Interaction_MakeSelection;
+      };
+      class GoBackIcon: RscInteractionHelperIcon{
+        idc = 1201;
+        text = "AGM_interaction\UI\mouse_right_ca.paa";
+        y = 19.5 * GUI_GRID_H;
+      };
+      class GoBackText: RscInteractionText{
+        idc = 1001;
+        y = 19 * GUI_GRID_H;
+        text = $STR_AGM_Interaction_Back;
+      };
+      class ScrollIcon: RscInteractionHelperIcon{
+        idc = 1202;
+        text = "AGM_interaction\UI\mouse_scroll_ca.paa";
+        y = 18.5 * GUI_GRID_H;
+      };
+      class ScrollText: RscInteractionText{
+        idc = 1002;
+        y = 18 * GUI_GRID_H;
+        text = $STR_AGM_Interaction_ScrollHint;
+      };
+    };
+  };
 };
