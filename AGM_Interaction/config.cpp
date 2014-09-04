25015d4b5c6b07c61b1f63ba53c4f749fa61726e
commy2 <d802099@drdrb.com>
2014-09-05 01:04:18 +0200

    fixed #1096, rotating fortifications

--- AGM_Interaction/config.cpp
+++ AGM_Interaction/config.cpp
@@ -146,14 +146,15 @@ class AGM_Core_Default_Keys {
     alt = 0;
   };
   class modifierKey {
-    displayName = $STR_AGM_Interaction_ModifierKey;
-    condition = "true";
+    displayName = "$STR_AGM_Interaction_ModifierKey";
+    condition = "";
     statement = "AGM_Modifier = 1;";
-    conditionUp = "AGM_Modifier > 0";
+    conditionUp = "";
     statementUp = "AGM_Modifier = 0;";
+    exceptions[] = {"AGM_Drag_isNotDragging"};
     key = 29;
     shift = 0;
-    control = 1;
+    control = 0;
     alt = 0;
   };
 };
