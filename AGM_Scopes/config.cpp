884a543dd26e697765d46e3b9b0b8e92bce88177
picard <nicolas.d.badano@gmail.com>
2014-10-15 20:53:13 -0300

    Zero sniper scopes to 300m. Fix #1309

--- AGM_Scopes/config.cpp
+++ AGM_Scopes/config.cpp
@@ -95,7 +95,7 @@ class CfgWeapons {
     class ItemInfo: InventoryOpticsItem_Base_F {
       class OpticsModes {
         class Snip {
-          discreteDistance[] = {};
+          discreteDistance[] = {300};
           discreteDistanceIndex = 0;
         };
       };
@@ -108,7 +108,7 @@ class CfgWeapons {
     class ItemInfo: InventoryOpticsItem_Base_F {
       class OpticsModes {
         class Snip {
-          discreteDistance[] = {};
+          discreteDistance[] = {300};
           discreteDistanceIndex = 0;
         };
       };
@@ -121,7 +121,7 @@ class CfgWeapons {
     class ItemInfo: InventoryOpticsItem_Base_F {
       class OpticsModes {
         class Snip {
-          discreteDistance[] = {};
+          discreteDistance[] = {300};
           discreteDistanceIndex = 0;
         };
       };
