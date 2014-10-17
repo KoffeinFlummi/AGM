26a862a77958bcd4653ce01325a333b64bdfda18
commy2 <d802099@drdrb.com>
2014-10-17 10:16:20 +0200

    nil

--- AGM_Core/HintConfig.hpp
+++ AGM_Core/HintConfig.hpp
@@ -56,7 +56,7 @@ class RscTitles {
 		idd = -1;
 		class controls {
 			class CameraView: RscMapControl {
-				onDraw = "if (cameraView != uiNamespace getVariable 'AGM_EventHandler_CameraMode') then {uiNamespace setVariable ['AGM_EventHandler_CameraMode', cameraView]; {[uiNamespace getVariable 'AGM_EventHandler_CameraMode'] call _x} count ((missionNamespace getVariable 'AGM_EventHandler_CameraMode') select 2);};";
+				onDraw = "if (cameraView != uiNamespace getVariable 'AGM_EventHandler_CameraMode') then {uiNamespace setVariable ['AGM_EventHandler_CameraMode', cameraView]; {[uiNamespace getVariable 'AGM_EventHandler_CameraMode'] call _x; nil} count ((missionNamespace getVariable 'AGM_EventHandler_CameraMode') select 2);};";
 				idc = -1;
 				w = 0;
 				h = 0;
@@ -66,7 +66,7 @@ class RscTitles {
 	class AGM_EventHandlerHelper2: AGM_Rsc_Display_Base {
 		class controls {
 			class MapMarkerCreated: RscMapControl {
-				onDraw = "if (count allMapMarkers != uiNamespace getVariable 'AGM_EventHandler_MapMarker') then {if (count allMapMarkers > uiNamespace getVariable 'AGM_EventHandler_MapMarker') then {{[allMapMarkers select count allMapMarkers - 1] call _x} count ((missionNamespace getVariable 'AGM_EventHandler_MapMarker') select 2);}; uiNamespace setVariable ['AGM_EventHandler_MapMarker', count allMapMarkers];};";
+				onDraw = "if (count allMapMarkers != uiNamespace getVariable 'AGM_EventHandler_MapMarker') then {if (count allMapMarkers > uiNamespace getVariable 'AGM_EventHandler_MapMarker') then {{[allMapMarkers select count allMapMarkers - 1] call _x; nil} count ((missionNamespace getVariable 'AGM_EventHandler_MapMarker') select 2);}; uiNamespace setVariable ['AGM_EventHandler_MapMarker', count allMapMarkers];};";
 				idc = -1;
 				w = 0;
 				h = 0;
