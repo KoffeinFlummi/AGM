62add015aae4917eab1091fbf1b5af3f853bc8bd
commy2 <d802099@drdrb.com>
2014-10-21 00:22:51 +0200

    rsc debug stuff

--- AGM_Core/config.cpp
+++ AGM_Core/config.cpp
@@ -268,6 +268,8 @@ class CfgFunctions {
   class AGM_Debug {
     class AGM_Debug {
       file = "AGM_Core\functions\Debug";
+      class logControls;
+      class logDisplays;
       class showUser;
     };
   };
