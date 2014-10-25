e47a199b644c4f3b98cb69029b6ffc93556ab699
commy2 <d802099@drdrb.com>
2014-10-25 12:00:12 +0200

    fied #1429

--- AGM_Hearing/config.cpp
+++ AGM_Hearing/config.cpp
@@ -172,3 +172,11 @@ class CfgWeapons {
     };
   };
 };
+
+// Setting up old stuff for A3
+class CfgAmmo {
+  class BulletBase;
+  class B_127x108_Ball: BulletBase {
+    audibleFire = 15;
+  };
+};
