d3d5dec43cbd3619460f33894369717cbb10f562
PabstMirror <elephantisaterriblemascot@gmail.com>
2014-11-25 03:13:58 -0600

    Add New NVG with varying quality

--- AGM_NightVision/config.cpp
+++ AGM_NightVision/config.cpp
@@ -1,7 +1,7 @@
 class CfgPatches {
   class AGM_NightVision {
     units[] = {};
-    weapons[] = {};
+    weapons[] = {"AGN_NVG_Gen1", "AGN_NVG_Gen2", "AGN_NVG_Gen3", "AGN_NVG_Gen4", "AGN_NVG_WIDE"};
     requiredVersion = 0.60;
     requiredAddons[] = {AGM_Core};
     version = "0.94.1";
@@ -57,5 +57,51 @@ class AGM_Core_Default_Keys {
     control = 0;
     alt = 1;
     allowHolding = 1;
+  };
+};
+
+class cfgWeapons {
+  class Binocular;
+  class NVGoggles: Binocular {
+    AGN_NightVision_grain = 0.75;
+    AGN_NightVision_blur = 0;
+  };
+  class NVGoggles_OPFOR: NVGoggles {};
+  class NVGoggles_INDEP: NVGoggles {};
+
+  class AGN_NVG_Gen1: NVGoggles_OPFOR {
+    author = "AGM Team";
+    modelOptics = "\A3\weapons_f\reticle\optics_night";
+    displayName = "$STR_AGM_NightVision_NVG_Gen1";
+    AGN_NightVision_grain = 2.25;
+    AGN_NightVision_blur = 0.22;
+  };
+  class AGN_NVG_Gen2: NVGoggles_INDEP {
+    author = "AGM Team";
+    modelOptics = "\A3\weapons_f\reticle\optics_night";
+    displayName = "$STR_AGM_NightVision_NVG_Gen2";
+    AGN_NightVision_grain = 1.5;
+    AGN_NightVision_blur = 0.11;
+  };
+  class AGN_NVG_Gen3: NVGoggles {
+    author = "AGM Team";
+    modelOptics = "\A3\weapons_f\reticle\optics_night";
+    displayName = "$STR_AGM_NightVision_NVG_Gen3";
+    AGN_NightVision_grain = 0.75;
+    AGN_NightVision_blur = 0.055;
+  };
+  class AGN_NVG_Gen4: NVGoggles {
+    author = "AGM Team";
+    modelOptics = "\A3\weapons_f\reticle\optics_night";
+    displayName = "$STR_AGM_NightVision_NVG_Gen4";
+    AGN_NightVision_grain = 0.0;
+    AGN_NightVision_blur = 0.0;
+  };
+  class AGN_NVG_WIDE: NVGoggles {
+    author = "AGM Team";
+    modelOptics = "\A3\weapons_f\reticle\optics_empty";
+    displayName = "$STR_AGM_NightVision_NVG_FullScreen";
+    AGN_NightVision_grain = 0.75;
+    AGN_NightVision_blur = 0.05;
   };
 };
