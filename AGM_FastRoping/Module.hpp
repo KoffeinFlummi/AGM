1d4c24fc6048b225e74c6c7f1804723e96bc88a3
Matthew Graham <me@mattgraham.me>
2015-02-25 10:55:12 -0600

    Added AGM FastRoping module hpp.

---
+++ AGM_FastRoping/Module.hpp
@@ -0,0 +1,28 @@
+class Module_F;
+class AGM_ModuleFastRoping: Module_F {
+	author = "AGM Team";
+	category = "AGM";
+	displayName = "Fast Roping System";
+	function = "AGM_FastRoping_fnc_module";
+	scope = 2;
+	isGlobal = 1;
+  icon = "\AGM_FastRoping\UI\IconFastRoping_ca.paa";
+	class Arguments {
+		class RequireCrew {
+			displayName = "Require Vehicle Crew?";
+			description = "Require vehicle crew to drop/cut ropes? Default: No";
+			typeName = "BOOL";
+			class values {
+				class Yes {
+					name = "Yes";
+					value = 1;
+				};
+				class No {
+					default = 1;
+					name = "No";
+					value = 0;
+				};
+			};
+		};
+	};
+};
